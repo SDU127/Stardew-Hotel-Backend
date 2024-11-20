15e66f03f8853d35510287da9b26c81fc1f6237b
Excarii <buxuan_1601@163.com>
2024-11-20 22:55:33 +0800

    :art: 改名

--- base-service/src/main/java/com/sdu127/BaseApplication.java
+++ base-service/src/main/java/com/sdu127/BaseApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PicApplication {
+public class BaseApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PicApplication.class, args);
+        SpringApplication.run(BaseApplication.class, args);
     }
 
 }
