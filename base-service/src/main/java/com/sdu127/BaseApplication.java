1a8422e5f701fbf4e20ad641a10cefcb9b46ebca
Excarii <buxuan_1601@163.com>
2024-12-01 15:35:57 +0800

    :art: 添加事务注解

--- base-service/src/main/java/com/sdu127/BaseApplication.java
+++ base-service/src/main/java/com/sdu127/BaseApplication.java
@@ -3,7 +3,9 @@ package com.sdu127;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @EnableFeignClients
 @SpringBootApplication
 public class BaseApplication {
